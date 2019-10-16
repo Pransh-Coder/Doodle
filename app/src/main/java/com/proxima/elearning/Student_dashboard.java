ae7f2a61939e51b8350a45f577a6cfe30eed3ab5
Pransh-Coder <rastogipransh4@gmail.com>
2019-10-16 13:45:01 +0530

    Student_Dashboard Activity created

---
+++ app/src/main/java/com/proxima/elearning/Student_dashboard.java
@@ -0,0 +1,15 @@
+package com.proxima.elearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Student_dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_dashboard);
+
+    }
+}
