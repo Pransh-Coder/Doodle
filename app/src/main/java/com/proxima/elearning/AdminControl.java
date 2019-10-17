c91f0cf5515791848c80978844e34348b99ed2f3
Ashish1414 <ashubansal.ashishbansal@gmail.com>
2019-10-18 02:48:47 +0530

    add student and add faculty done

--- app/src/main/java/com/proxima/elearning/AdminControl.java
+++ app/src/main/java/com/proxima/elearning/AdminControl.java
@@ -2,13 +2,48 @@ package com.proxima.elearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminControl extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_control);
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedPreferences = getSharedPreferences("Login",0);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.commit();
+                Intent intent = new Intent(AdminControl.this,Login.class);
+                startActivityForResult(intent, 0);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
+
+        findViewById(R.id.btnAddStudent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminControl.this,AddStudent.class);
+                startActivityForResult(intent,0);
+                overridePendingTransition(0,0);
+            }
+        });
+        findViewById(R.id.btnAddFaculty).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdminControl.this,AddFaculty.class);
+                startActivityForResult(intent,0);
+                overridePendingTransition(0,0);
+            }
+        });
     }
 }
